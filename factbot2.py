e999c95304c70f62227c61ff3a4c519af0b39a60
rigterw <rigterwessel@gmail.com>
2022-04-18 22:41:17 +0200

    Adds translation

--- factbot2.py
+++ factbot2.py
@@ -1,16 +1,18 @@
 from multiprocessing.connection import wait
+from googletrans import Translator
 import discord, datetime, asyncio
 from random import randrange as random
 
 
 client = discord.Client()
 nextfact = datetime.datetime(1,1,1)
+translator = Translator()
 
 
 
 @client.event
 async def on_connect():
-    nextfact = datetime.datetime.now()
+
     i = 0
     while i < 10:
         await checkFactTime()
@@ -35,15 +37,17 @@ async def checkFactTime():
             await asyncio.sleep(setTime())
 
 async def sendFact():
+    fact = "hello world"
+    feitje = translator.translate(fact, "nl")
     channel = client.get_channel(715680206609973319)
     print(channel)
-    await channel.send("go")
+    await channel.send(feitje.text)
 
 def setTime():
     currentday = datetime.datetime.now()
-    nextfact = datetime.datetime(currentday.year, currentday.month, currentday.day, random(0,23), random(0,59))
+    tomorrow = currentday.day + 1
+    nextfact = datetime.datetime(currentday.year, currentday.month, tomorrow, random(0,23), random(0,59))
     waitTime = (nextfact-currentday).total_seconds()
-    #print(waitTime)
     return waitTime
 
 
@@ -51,6 +55,6 @@ def setTime():
 
 
 
-client.run('OTY0OTM3MTU1MzI1NjA4MDg4.Ylr5wQ.tnhyXaJCVP7G-UnOElaPm5yaId4')
+client.run('OTY0OTM3MTU1MzI1NjA4MDg4.Ylr5wQ.aI_4fq6rTHLDOpqZwjX4It6tXP8')
 print("run")
 
